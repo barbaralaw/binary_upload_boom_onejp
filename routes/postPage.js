bc143d36d68b16c41fee37b33511503f8000c292
throne.yash@gmail.com <throne.yash@gmail.com>
2021-05-03 18:47:47 -0600

    Likes and Dislikes button fixed

--- routes/postPage.js
+++ routes/postPage.js
@@ -5,4 +5,8 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 router.get("/:id", ensureAuth, postPageController.getPostPage);
 
+router.put("/likePost/:id", postPageController.likePost)
+
+router.put("/dislikePost/:id", postPageController.dislikePost)
+
 module.exports = router;
