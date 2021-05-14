10f7fbe41d68205af8c1898d0cc63bec5a883515
throne.yash@gmail.com <throne.yash@gmail.com>
2021-05-14 13:29:15 -0600

    Now the user can like and dislike posts from the feeds page

--- controllers/feed.js
+++ controllers/feed.js
@@ -12,5 +12,118 @@ module.exports = {
         }catch(err){
             console.log(err)
         }
+    },
+    likePost: async(req, res) => {
+        const post = await Feed.findById(req.params.id)
+        // console.log(post)
+        if(post.likes.includes(req.user.id)){
+            try {
+                await Feed.findOneAndUpdate(
+                    { _id: req.params.id},
+                    {
+                        $pull: { likes: req.user.id },
+                    },
+                    {
+                        new: true,
+                    }
+                )
+                console.log("Likes -1");
+                res.redirect(`/feed`);
+                
+            } catch (err) {
+                console.log(err);
+            }
+        }else if(post.dislikes.includes(req.user.id)){
+            try {
+                await Feed.findOneAndUpdate(
+                    { _id: req.params.id},
+                    {
+                        $pull: { dislikes: req.user.id },
+                        $push: { likes: req.user.id },
+                    },
+                    {
+                        new: true,
+                    }
+                )
+                console.log("Likes -1");
+                res.redirect(`/feed`);
+                
+            } catch (err) {
+                console.log(err);
+            }
+        }else{
+            try {
+                await Feed.findOneAndUpdate(
+                    { _id: req.params.id},
+                    {
+                        $push: { likes: req.user.id },
+                    },
+                    {
+                        new: true,
+                    }
+                )
+            console.log("Likes +1");
+            res.redirect(`/feed`);
+            
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    },
+    dislikePost: async(req, res) => {
+        const post = await Feed.findById(req.params.id)
+        if(post.dislikes.includes(req.user.id)){
+            try {
+                await Feed.findOneAndUpdate(
+                    { _id: req.params.id},
+                    {
+                        $pull: { dislikes: req.user.id },
+                    },
+                    {
+                        new: true,
+                    }
+                )
+                console.log("dislikes -1");
+                res.redirect(`/feed`);
+                
+            } catch (err) {
+                console.log(err);
+            }
+        }else if(post.likes.includes(req.user.id)){
+            try {
+                await Feed.findOneAndUpdate(
+                    { _id: req.params.id},
+                    {
+                        $pull: { likes: req.user.id },
+                        $push: { dislikes: req.user.id }
+                    },
+                    {
+                        new: true,
+                    }
+                )
+                console.log("dislikes -1");
+                res.redirect(`/feed`);
+                
+            } catch (err) {
+                console.log(err);
+            }
+        }else{
+            try {
+                await Feed.findOneAndUpdate(
+                    { _id: req.params.id},
+                    {
+                        $push: { dislikes: req.user.id },
+                    },
+                    {
+                        new: true,
+                    }
+                )
+            console.log("dislikes +1");
+            res.redirect(`/feed`);
+            
+            } catch (err) {
+                console.log(err);
+            }
+        }
     }
 }
